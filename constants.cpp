03c7eeebe1aef8f92bbfdbd2109e408ef87c8cf9
Sadman Sakib <sakibsadman035@gmail.com>
2025-09-05 16:12:53 +0600

    Release_v0.3 (bug fix)

--- constants.cpp
+++ constants.cpp
@@ -6,24 +6,19 @@ const float PLAYER_X = 0.0f;
 const float PLAYER_Y = -80.0f;
 const float PLAYER_RADIUS = 8.2f;
 
-const int TOTAL_LIVES = 3;
-
 const float GUN_X = 9.0f;
 const float GUN_Y = -80.0f;
 
 const float BOMB_RADIUS = 2.0f;
 const float BOMB_SPEED = 0.15f;
 float BOMB_EXPLOSION_MAX_RADIUS = 6.0f;
-
-
-const float BULLET_SPEED = 4.0f;
-
 const int BOMB_WAVE_TIME_SEC = 10;
 
-
+const float BULLET_SPEED = 4.0f;
 
 const string WORDS_FILE_PATH = "words.txt";
 const int WORD_LIST_LENGTH = 431;
 
+const int TOTAL_LIVES = 3;
 int livesLeft = 3;
 int score = 0;
